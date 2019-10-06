f7e6db4df148462186c1545819700638560c98e1
Jonathan1010 <51387003+Jonathan1010@users.noreply.github.com>
2019-10-05 22:23:29 -0500

    Add files via upload

---
+++ Saludar/Saludo1.java
@@ -0,0 +1,51 @@
+package saludar;
+
+public class Saludo1 {
+    String lenguaje;
+    String Gestos;
+    String Acciondelcuerpo;
+    String idioma;
+
+    public String getLenguaje() {
+        return lenguaje;
+    }
+
+    public void setLenguaje(String lenguaje) {
+        this.lenguaje = lenguaje;
+    }
+
+    public String getGestos() {
+        return Gestos;
+    }
+
+    public void setGestos(String Gestos) {
+        this.Gestos = Gestos;
+    }
+
+    public String getAcciondelcuerpo() {
+        return Acciondelcuerpo;
+    }
+
+    public void setAcciondelcuerpo(String Acciondelcuerpo) {
+        this.Acciondelcuerpo = Acciondelcuerpo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+    
+    //persona
+    public Saludo1 saludar(boolean conozco, String genero, int edad){
+    
+    }
+    //telefono
+    public Saludo1 saludar(String genero, boolean conozco){
+        
+    }
+    //waths app sin conocer
+    
+}

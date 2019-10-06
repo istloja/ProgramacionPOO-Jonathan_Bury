acd655e4d6472b4643fe99de59c63e6a7cf6bc13
Jonathan1010 <51387003+Jonathan1010@users.noreply.github.com>
2019-10-05 22:42:44 -0500

    Add files via upload

---
+++ practicas/Casilla.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Casilla {
+    String muro;
+    String vacia;
+    String punto; 
+    String punto_especial;
+
+    public String getMuro() {
+        return muro;
+    }
+
+    public void setMuro(String muro) {
+        this.muro = muro;
+    }
+
+    public String getVacia() {
+        return vacia;
+    }
+
+    public void setVacia(String vacia) {
+        this.vacia = vacia;
+    }
+
+    public String getPunto() {
+        return punto;
+    }
+
+    public void setPunto(String punto) {
+        this.punto = punto;
+    }
+
+    public String getPunto_especial() {
+        return punto_especial;
+    }
+
+    public void setPunto_especial(String punto_especial) {
+        this.punto_especial = punto_especial;
+    }
+    //metodos
+    /*o Obtener el tipo de una casilla.
+o Obtener la representación de la casilla en forma de cadena de texto.
+o Fijar o cambiar el tipo de una casilla. */
+    
+    
+}

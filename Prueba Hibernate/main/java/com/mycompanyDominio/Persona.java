973114450018a9445ea2c73baa0a30109cb101dc
Jonathan1010 <51387003+Jonathan1010@users.noreply.github.com>
2019-10-05 22:44:59 -0500

    Add files via upload

---
+++ Prueba Hibernate/main/java/com/mycompanyDominio/Persona.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompanyDominio;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Persona {
+    private int idPersona;
+    private String nombre;
+    private String apellido;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+    
+}
